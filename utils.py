67d9abd708f0769a1151d04dcc342ebb55879227
Jack Wimberley <jack.wimberley@cern.ch>
2015-12-31 10:05:53 -0500

    - Adding statevent functions for computing effective number (sumw)/sumw2   of events - Adding plots of calibrations to calibration_probs - Using scikit-learn 0.17, so that weights can be used in logistic   calibration with 'sag' solver

--- utils.py
+++ utils.py
@@ -33,11 +33,30 @@ def get_events_number(data, id_column='event_id'):
     weights = numpy.bincount(data_ids, weights=data.N_sig_sw) / numpy.bincount(data_ids)
     return numpy.sum(weights)
 
+def get_statevents_number(data, id_column='event_id'):
+    """
+    :return: effective number of B events, (sumw)^2/sumw2
+    """
+    _, data_ids = numpy.unique(data[id_column], return_inverse=True)
+    weights = numpy.bincount(data_ids, weights=data.N_sig_sw) / numpy.bincount(data_ids)
+    sumw = numpy.sum(weights)
+    sumw2 = numpy.sum(weights*weights)
+    effnum = sumw*sumw/sumw2
+    return effnum
+
 def get_N_B_events():
     '''
     :return: number of B decays (sum of sWeight in initial root file) 
     '''
     N_B_decays = 7.42867714256286621e+05
+    return N_B_decays
+
+def get_N_B_statevents():
+    '''
+    :return: effective number of B decays (sum of sWeight^2 / sum of squared sWeights in initial root file)
+    '''
+    # not currently known; probably about this much
+    N_B_decays = 1.1*7.42867714256286621e+05
     return N_B_decays
 
     
@@ -71,7 +90,7 @@ def plot_flattened_probs(probs, labels, weights, label=1, check_input=True):
     return flattener
 
 
-def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_column=None, threshold=0., symmetrize=False):
+def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_column=None, threshold=0., symmetrize=False, plot=False):
     """
     Bootstrap isotonic calibration: 
      * randomly divide data into train-test
@@ -99,6 +118,7 @@ def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_co
                 probs, labels, weights, train_size=0.5)
         iso_est = IsotonicRegression(y_min=0, y_max=1, out_of_bounds='clip')
         if symmetrize:
+            train_weights = 0.5*train_weights;
             iso_est.fit(numpy.r_[train_probs, 1-train_probs], 
                         numpy.r_[train_labels > 0, train_labels <= 0],
                         numpy.r_[train_weights, train_weights])
@@ -106,6 +126,15 @@ def bootstrap_calibrate_prob(labels, weights, probs, n_calibrations=30, group_co
             iso_est.fit(train_probs, train_labels, train_weights)
             
         probs_calib = iso_est.transform(test_probs)
+
+        if plot:
+            plt.figure(1,figsize=(6,5))
+            plt.scatter(train_probs, train_labels, color='black', zorder=20)
+            X_test = numpy.linspace(0.001,0.999,500)
+            y_test = iso_est.transform(X_test)
+            plt.plot(X_test, y_test, color='blue', linewidth=3)
+            plt.show()
+
         alpha = (1 - 2 * probs_calib) ** 2
         aucs.append(roc_auc_score(test_labels, test_probs, sample_weight=test_weights))
         D2_array.append(numpy.average(alpha, weights=test_weights))
@@ -159,7 +188,7 @@ def result_table(tagging_efficiency, tagging_efficiency_delta, D2, auc, name='mo
     return pandas.DataFrame(result)
 
 
-def calibrate_probs(labels, weights, probs, logistic=False, random_state=11, threshold=0., return_calibrator=False, symmetrize=False):
+def calibrate_probs(labels, weights, probs, logistic=False, random_state=11, threshold=0., return_calibrator=False, symmetrize=False, inEtaSpace=False, plot=False):
     """
     Calibrate output to probabilities using 2-folding to calibrate all data
     
@@ -168,55 +197,168 @@ def calibrate_probs(labels, weights, probs, logistic=False, random_state=11, thr
     :param weights: numpy.array of shape [n_samples]
     :param threshold: float, to set labels 0/1 
     :param logistic: bool, use logistic or isotonic regression
+    :param inEtaSpace: bool, do calibration in eta between 0 and 0.5    
     :param symmetrize: bool, do symmetric calibration, ex. for B+, B-
-    
+
     :return: calibrated probabilities
     """
     labels = (labels > threshold) * 1
     ind = numpy.arange(len(probs))
     ind_1, ind_2 = train_test_split(ind, random_state=random_state, train_size=0.5)
     
-    calibrator = LogisticRegression(C=100) if logistic else IsotonicRegression(y_min=0, y_max=1, out_of_bounds='clip')
+    calibrator = LogisticRegression(C=100,solver='sag') if logistic else IsotonicRegression(y_min=0, y_max=1, out_of_bounds='clip')
     est_calib_1, est_calib_2 = clone(calibrator), clone(calibrator)
     probs_1 = probs[ind_1]
     probs_2 = probs[ind_2]
+    flav_1 = labels[ind_1]
+    flav_2 = labels[ind_2]
+    w1 = weights[ind_1]
+    w2 = weights[ind_2]
     
-    if logistic:
-        probs_1 = numpy.clip(probs_1, 0.001, 0.999)
-        probs_2 = numpy.clip(probs_2, 0.001, 0.999)
-        probs_1 = logit(probs_1)[:, numpy.newaxis]
-        probs_2 = logit(probs_2)[:, numpy.newaxis]
-        if symmetrize:
-            est_calib_1.fit(numpy.r_[probs_1, 1-probs_1], 
-                            numpy.r_[labels[ind_1] > 0, labels[ind_1] <= 0])
-            est_calib_2.fit(numpy.r_[probs_2, 1-probs_2], 
-                            numpy.r_[labels[ind_2] > 0, labels[ind_2] <= 0])
-        else:
-            est_calib_1.fit(probs_1, labels[ind_1])
-            est_calib_2.fit(probs_2, labels[ind_2])
+    # Turn 0-1 B+/B- space into 0-0.5 mistagged/tagged space
+    x1 = x2 = y1 = y2 = dllx1 = dllx2 = []
+    dil_1 = 2*probs_1 - 1 # 0 => -1; 0.5 => 0; 1 => 1
+    dil_2 = 2*probs_2 - 1
+    tag_1 = numpy.sign(dil_1) # - => -1; + => +1
+    tag_2 = numpy.sign(dil_2)
+    eta_1 = 0.5*(1-numpy.abs(dil_1)) # 0 => 0; 0.5 => 0.5; 1 => 0
+    eta_2 = 0.5*(1-numpy.abs(dil_2))
+    if False:
+        plt.figure(1,figsize=(6,5))
+        plt.scatter(probs_1,eta_1)
+        plt.show()
+    
+    if inEtaSpace:
+        x1 = eta_1
+        x2 = eta_2
+        y1 = tag_1 != 2*flav_1-1
+        y2 = tag_2 != 2*flav_2-1
     else:
         if symmetrize:
-            est_calib_1.fit(numpy.r_[probs_1, 1-probs_1], 
-                            numpy.r_[labels[ind_1] > 0, labels[ind_1] <= 0],
-                            numpy.r_[weights[ind_1], weights[ind_1]])
-            est_calib_2.fit(numpy.r_[probs_2, 1-probs_2], 
-                            numpy.r_[labels[ind_2] > 0, labels[ind_2] <= 0],
-                            numpy.r_[weights[ind_2], weights[ind_2]])
+            x1 = numpy.r_[probs_1, 1-probs_1]
+            x2 = numpy.r_[probs_2, 1-probs_2]
+            y1 = numpy.r_[flav_1 > 0, flav_1 <= 0]
+            y2 = numpy.r_[flav_2 > 0, flav_2 <= 0]
+            w1 = 0.5*w1
+            w2 = 0.5*w2
+            w1 = numpy.r_[w1,w1]
+            w2 = numpy.r_[w2,w2]
         else:
-            est_calib_1.fit(probs_1, labels[ind_1], weights[ind_1])
-            est_calib_2.fit(probs_2, labels[ind_2], weights[ind_2])
+            x1 = probs_1
+            x2 = probs_2
+            y1 = flav_1>0
+            y2 = flav_2>0
+
+            
+    ## If logistic regression, change x to logit(x)
+    if logistic:
+        if inEtaSpace:
+              x1 = numpy.clip(x1, 0.00001, 0.49999)
+              x2 = numpy.clip(x2, 0.00001, 0.49999)
+        else:
+              x1 = numpy.clip(x1, 0.00001, 0.99999)
+              x2 = numpy.clip(x2, 0.00001, 0.99999)
+        dllx1 = logit(x1)[:, numpy.newaxis]
+        dllx2 = logit(x2)[:, numpy.newaxis]
+    
+    # Do the fit
+    if logistic:
+        est_calib_1.fit(dllx1,y1,sample_weight=w1)
+        est_calib_2.fit(dllx2,y2,sample_weight=w2)
+    else:
+        est_calib_1.fit(x1,y1,w1)
+        est_calib_2.fit(x2,y2,w2)
         
-    calibrated_probs = numpy.zeros(len(probs))
+    # Plots
+    if plot:
+        X_test = []
+        if inEtaSpace:
+            X_test = numpy.linspace(0.001,0.499,500)
+        else:
+            X_test = numpy.linspace(0.001,0.999,500)
+            
+        c1 = c2 = []
+        if logistic:
+            dllX_test = logit(X_test)[:, numpy.newaxis]
+            c1 = est_calib_1.predict_proba(dllX_test)[:, 1]
+            c2 = est_calib_2.predict_proba(dllX_test)[:, 1]
+        else:
+            c1 = est_calib_1.transform(X_test)
+            c2 = est_calib_2.transform(X_test)
+                
+        tryN = 250
+
+        X1 = x1.ravel()
+        N = tryN
+        groups1 = None
+        success = False
+        while not success:
+            try:
+                groups1 = pandas.qcut(X1,N,range(N))
+                success = True
+            except ValueError:
+                N -= 1
+        plotd1 = pandas.DataFrame({'X' : numpy.multiply(X1,w1), 'g' : groups1, 'y' : numpy.multiply(y1,w1), 'w' : w1 })
+        grouped1 = plotd1.groupby('g')
+        gsum1 = grouped1.sum()
+        gsum1['X'] = gsum1['X'] / gsum1['w']
+        gsum1['y'] = gsum1['y'] / gsum1['w']
+
+        X2 = x2.ravel()
+        N = tryN
+        groups2 = None
+        success = False
+        while not success:
+            try:
+                groups2 = pandas.qcut(X2,N,range(N))
+                success = True
+            except ValueError:
+                N -= 1
+        plotd2 = pandas.DataFrame({'X' : numpy.multiply(X2,w2), 'g' : groups2, 'y' : numpy.multiply(y2,w2), 'w' : w2 })
+        grouped2 = plotd2.groupby('g')
+        gsum2 = grouped2.sum()
+        gsum2['X'] = gsum2['X'] / gsum2['w']
+        gsum2['y'] = gsum2['y'] / gsum2['w']
+
+        plt.figure(1,figsize=(12,5))
+        plt.subplot(1,2,1)
+        plt.scatter(x1.ravel(), y1, color='black', zorder=20)
+        plt.scatter(gsum1['X'], gsum1['y'], color='red', zorder=20)
+        plt.plot(X_test, c1, color='blue', linewidth=3)
+        plt.subplot(1,2,2)
+        plt.scatter(x2.ravel(), y2, color='black', zorder=20)
+        plt.scatter(gsum2['X'], gsum2['y'], color='red', zorder=20)
+        plt.plot(X_test, c2, color='blue', linewidth=3)
+        plt.show()
+    
+    # Cross validate
+    p1 = p2 = []
     if logistic:
-        calibrated_probs[ind_1] = est_calib_2.predict_proba(probs_1)[:, 1]
-        calibrated_probs[ind_2] = est_calib_1.predict_proba(probs_2)[:, 1]
+        p1 = est_calib_2.predict_proba(dllx1)[:, 1]
+        p2 = est_calib_1.predict_proba(dllx2)[:, 1]
     else:
-        calibrated_probs[ind_1] = est_calib_2.transform(probs_1)
-        calibrated_probs[ind_2] = est_calib_1.transform(probs_2)
+        p1 = est_calib_2.transform(x1)
+        p2 = est_calib_1.transform(x2)
+
+    # Transform back to flav space
+    if inEtaSpace:
+        pdil_1 = (1-2*p1)*tag_1
+        pdil_2 = (1-2*p2)*tag_2
+        p1 = 0.5*(1+pdil_1)
+        p2 = 0.5*(1+pdil_2)
+
+    # Save
+    calibrated_probs = numpy.zeros(len(probs))
+    calibrated_probs[ind_1] = p1
+    calibrated_probs[ind_2] = p2
+
+    # Return
+    alpha = (1 - 2 * calibrated_probs) ** 2
+    D2 = numpy.average(alpha,weights=weights)
     if return_calibrator:
-        return calibrated_probs, (est_calib_1, est_calib_2)
+        return calibrated_probs, D2, (est_calib_1, est_calib_2)
     else:
-        return calibrated_probs
+        return calibrated_probs, D2
 
 
 def calculate_auc_with_and_without_untag_events(Bsign, Bprobs, Bweights):
@@ -270,7 +412,7 @@ def compute_B_prob_using_part_prob(data, probs, weight_column='N_sig_sw', event_
     return result_label, result_weight, expit(result_logprob), result_event_id
 
 
-def get_B_data_for_given_part(estimator, datasets, logistic=True, sign_part_column='signTrack', part_name='track',
+def get_B_data_for_given_part(estimator, datasets, logistic=True, inEtaSpace=False, sign_part_column='signTrack', part_name='track',
                               random_state=11, normed_signs=False):
     """
     Predict probabilities for event parts, calibrate it and compute B data.
@@ -279,15 +421,16 @@ def get_B_data_for_given_part(estimator, datasets, logistic=True, sign_part_colu
     :param estimator: REP classifier, already trained model.
     :param datasets: list of pandas.DataFrames to predict.
     :param logistic: bool, use logistic or isotonic regression for part (track/vertex) probabilities calibration
-    :param sign_part_column: column for part sign in data
+    :param inEtaSpace: bool, do calibration in eta between 0 and 0.5 
     :param part_name: part data name for plots 
     
     :return: B sign, weight, p(B+), event id and full auc (with untag events) 
     """
     # Calibration p(track/vertex same sign|B)
     data_calib, part_probs = predict_by_estimator(estimator, datasets)
-    part_probs_calib = calibrate_probs(data_calib.label.values, data_calib.N_sig_sw.values, part_probs, 
-                                       logistic=logistic, random_state=random_state)
+    part_probs_calib, D2 = calibrate_probs(data_calib.label.values, data_calib.N_sig_sw.values, part_probs, 
+                                       logistic=logistic, inEtaSpace=inEtaSpace, random_state=random_state)
+
     plt.figure(figsize=[18, 5])
     plt.subplot(1,3,1)
     plt.hist(part_probs[data_calib.label.values == 0], bins=60, normed=True, alpha=0.3, label='os')
@@ -319,7 +462,7 @@ def get_B_data_for_given_part(estimator, datasets, logistic=True, sign_part_colu
 
 
 def get_result_with_bootstrap_for_given_part(tagging_efficiency, tagging_efficiency_delta, estimator,
-                                             datasets, name, logistic=True, n_calibrations=30,
+                                             datasets, name, logistic=True, inEtaSpace=False, n_calibrations=30,
                                              sign_part_column='signTrack', part_name='track',
                                              random_state=11, normed_signs=False):
     """
@@ -331,22 +474,32 @@ def get_result_with_bootstrap_for_given_part(tagging_efficiency, tagging_efficie
     :param datasets: list of pandas.DataFrames to predict.
     :param name: str, name of model
     :param logistic: bool, use logistic or isotonic regression for part (track/vertex) probabilities calibration
+    :param inEtaSpace: bool, do calibration in eta between 0 and 0.5
     :param sign_part_column: column for part sign in data
     :param part_name: part data name for plots 
     
     :return: pandas.DataFrame with only one row, describing result_table
     """
-    Bsign, Bweight, Bprob, Bevent, auc_full = get_B_data_for_given_part(estimator, datasets, logistic=logistic, 
+    Bsign, Bweight, Bprob, Bevent, auc_full = get_B_data_for_given_part(estimator, datasets, logistic=logistic, inEtaSpace=inEtaSpace,
                                                                         sign_part_column=sign_part_column, 
                                                                         part_name=part_name, random_state=random_state,
                                                                         normed_signs=normed_signs)    
     # Compute p(B+) calibrated with bootstrap
-    D2, aucs = bootstrap_calibrate_prob(Bsign, Bweight, Bprob, n_calibrations=30)
-    print 'mean AUC after calibration:', numpy.mean(aucs), numpy.var(aucs)
+    _, D2log = calibrate_probs(Bsign, Bweight, Bprob, logistic=True,inEtaSpace=True,symmetrize=True,plot=True)
+    print 'D2 using logistic calibration: ', D2log
+    _, D2iso = calibrate_probs(Bsign, Bweight, Bprob, logistic=False,inEtaSpace=True,symmetrize=True,plot=True)
+    print 'D2 using isotonic calibration: ', D2iso
+    _, D2log = calibrate_probs(Bsign, Bweight, Bprob, logistic=True,inEtaSpace=False,symmetrize=True,plot=True)
+    print 'D2 using B+/- logistic calibration: ', D2log
+    _, D2iso = calibrate_probs(Bsign, Bweight, Bprob, logistic=False,inEtaSpace=False,symmetrize=True,plot=True)
+    print 'D2 using B+/B- isotonic calibration: ', D2iso
+    D2, aucs = bootstrap_calibrate_prob(Bsign, Bweight, Bprob, n_calibrations=30,plot=False)
+    print 'bootstrap mean D2 after calibration:', numpy.mean(D2), numpy.var(aucs)
+    print 'bootstrap mean AUC after calibration:', numpy.mean(aucs), numpy.var(aucs)
     return result_table(tagging_efficiency, tagging_efficiency_delta, D2, auc_full, name)
 
 
-def prepare_B_data_for_given_part(estimator, datasets, logistic=True, sign_part_column='signTrack', part_name='track', 
+def prepare_B_data_for_given_part(estimator, datasets, logistic=True, inEtaSpace=False,sign_part_column='signTrack', part_name='track', 
                                   random_state=11, normed_signs=False):
     """
     Prepare B data for event parts (track/vetex) for further combination of track-based and vertex-based taggers:
@@ -356,13 +509,14 @@ def prepare_B_data_for_given_part(estimator, datasets, logistic=True, sign_part_
     :param datasets: list of pandas.DataFrames to predict.
     :param name: str, name of model
     :param logistic: bool, use logistic or isotonic regression for part (track/vertex) probabilities calibration
+    :param inEtaSpace: bool, do calibration in eta between 0 and 0.5    
     :param sign_part_column: column for part sign in data
     :param part_name: part data name for plots 
     
     :return: pandas.DataFrame with keys: `event_id` - B id, `Bweight` - B weight, `{part_name}_relation_prob` p(B+) / (1 - p(B+)) for given part, `Bsign` - sign B
     """
     
-    Bsign, Bweight, Bprob, Bevent, auc_full = get_B_data_for_given_part(estimator, datasets, logistic=logistic, 
+    Bsign, Bweight, Bprob, Bevent, auc_full = get_B_data_for_given_part(estimator, datasets, logistic=logistic, inEtaSpace=inEtaSpace,
                                                                         sign_part_column=sign_part_column, 
                                                                         part_name=part_name, random_state=random_state,
                                                                         normed_signs=normed_signs)    
@@ -417,3 +571,4 @@ def compute_mistag(Bprobs, Bsign, Bweight, chosen, uniform=True, bins=None, labe
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim(-0.05, 0.55), plt.ylim(-0.05, 0.55)
     plt.grid()
+
